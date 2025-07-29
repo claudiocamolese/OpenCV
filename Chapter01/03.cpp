ac8d9798e9fafd918ffbf76d5ad839e0f96ff331
claudiocamolese <claudiocamolese@gmail.com>
2025-07-29 18:48:38 +0200

    04 file

--- Chapter01/03.cpp
+++ Chapter01/03.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <fstream>
 
-/*This code demonstrates how to read a video file and display it frame by frame.
+/*
+This code demonstrates how to read a video file and display it frame by frame.
+Extracts from a video: numer of frames and their sizes.
 It also includes a trackbar to control the position of the video playback.
 */
 
@@ -21,7 +23,8 @@ int g_dontset = 0;
 
 cv::VideoCapture g_cap;
 
-void onTrackbarSlide(int pos, void*){
+void onTrackbarSlide(int pos, void* _){
+    // If the slider is moved, set the position of the video capture
     g_cap.set(cv::CAP_PROP_POS_FRAMES, pos);
     if(!g_dontset){
         g_run =1;
@@ -33,29 +36,37 @@ int main(int argc, char** argv)
 {
     cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_SILENT);
     
-    cv::namedWindow("Example2_4", cv::WINDOW_AUTOSIZE);
-    g_cap.open(std::string(argv[1])); // a string must be passed
+    // Create a window to display the video
+    cv::namedWindow("Example", cv::WINDOW_AUTOSIZE);
+    g_cap.open(std::string(argv[1])); // a string of the video path must be passed
 
     // define variables. Use of (int) to cast the double valure returned by .get() 
+    //CAP_PROP_FRAME_COUNT returns the number of frames in the video
+    //CAP_PROP_FRAME_WIDTH and CAP_PROP_FRAME_HEIGHT return the width and height of the frames
     int frames = (int) g_cap.get(cv::CAP_PROP_FRAME_COUNT);
     int tmpw = (int) g_cap.get(cv::CAP_PROP_FRAME_WIDTH);
     int tmph = (int) g_cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+
+    // Create a pair to hold the width and height of the video frames
     std::pair<int, int> size = std::make_pair(tmpw, tmph);
 
     // print values video
     std::cout << "Video has: " << frames<< "frames, and sizes: (" << size.first<< "," << size.second<<")"<< std::endl;
 
-    cv::createTrackbar("Position", "Example2_4", &g_slider_position, frames, onTrackbarSlide);
+    // Create a trackbar to control the position of the video playback
+    // The trackbar will be linked to the "Example" window, with a range from 0 to the number of frames in the video
+    // The callback function onTrackbarSlide will be called when the slider is moved
+    cv::createTrackbar("Position", "Example", &g_slider_position, frames, onTrackbarSlide);
     cv::Mat frame;
 
-    //eternal loop
+    // eternal loop
     for(;;) {
         if( g_run != 0) {
             g_cap >> frame; if(frame.empty()) break;
             int current_pos = (int)g_cap.get(cv::CAP_PROP_POS_FRAMES);
             g_dontset = 1;
-            cv::setTrackbarPos("Position", "Example2_4", current_pos);
-            cv::imshow( "Example2_4", frame );
+            cv::setTrackbarPos("Position", "Example", current_pos);
+            cv::imshow( "Example", frame );
             g_run-=1;
     }
 
---
+++ Chapter01/04.cpp
@@ -0,0 +1,75 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <fstream>
+
+/*
+This code demonstrates how to apply different types of blurring filters to an image.
+It allows the user to specify the type of blur filter to apply via command line arguments.
+The user can run the program with the image path and the desired filter type as arguments.
+Usage: ./blur_image <image_path> <filter_type>
+Where <filter_type> can be:
+1. Gaussian Blur
+2. Average Blur
+3. Median Blur
+*/
+
+
+void SmoothBlur(const cv::Mat& img, int value){
+    /**
+     * @brief Apply to the img the selected blur filter 
+     * 
+     * @param img is the original img
+     * @param value is the int number corresponding to the blur filter  
+     */
+    std::string windowName = "Blurred img - Filter " + std::to_string(value);
+    cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
+    cv::Mat out;
+
+    switch (value)
+    {
+    case 1:
+        cv::GaussianBlur(img, out, cv::Size(5,5), 3,3);
+        break;
+    
+    case 2:
+        cv::blur(img, out, cv::Size(3,3), cv::Point(-1,-1));
+        break;
+    
+    case 3:
+        cv::medianBlur(img, out, 3);
+        break;
+    default:
+        std::cerr << "Filter not recognized " << value << ". Used GaussianBlur as defualt." << std::endl;
+        cv::GaussianBlur(img, out, cv::Size(5,5), 3,3);
+        break;
+    }
+
+    cv::imshow(windowName, out);
+}
+
+int main(int argc, char** argv)
+{
+    cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_SILENT);
+
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <image_path> <filter1> [filter2] [filter3] ..." << std::endl;
+        return -1;
+    }
+
+    cv::Mat img = cv::imread(argv[1], -1);
+    if (img.empty()) {
+        std::cerr << "Image not found" << std::endl;
+        return -1;
+    }
+
+    cv::namedWindow("Original image", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Original image", img);
+
+    for (int i = 2; i < argc; ++i) {
+        int filterType = std::stoi(argv[i]);
+        SmoothBlur(img, filterType);
+    }
+
+    cv::waitKey(0);
+    return 0;
+}
